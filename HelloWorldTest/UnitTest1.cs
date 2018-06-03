ac14a3383e0cda9d18285f8feb209f28220ec66b
Hiren Bhavsar <hbhavsar01@hotmail.com>
2018-06-03 19:39:32 -0400

    added more tests

--- HelloWorldTest/UnitTest1.cs
+++ HelloWorldTest/UnitTest1.cs
@@ -9,7 +9,14 @@ namespace HelloWorldTest
         [TestMethod]
         public void TestMethod1()
         {
-            string text = "Hello World";
+            string text = "Hello World!";
+
+            Assert.AreEqual("Hello World!", text);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string text = "Hello USA";
 
             Assert.AreEqual("Hello World", text);
         }
