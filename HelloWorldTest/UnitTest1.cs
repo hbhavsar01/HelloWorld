c85dd89b6a11805ef9696fd73fbb9dc82c11465f
Hiren Bhavsar <hbhavsar01@hotmail.com>
2018-06-05 20:54:12 -0400

    changed second test

--- HelloWorldTest/UnitTest1.cs
+++ HelloWorldTest/UnitTest1.cs
@@ -16,7 +16,7 @@ namespace HelloWorldTest
         [TestMethod]
         public void TestMethod2()
         {
-            string text = "Hello World";
+            string text = "Hello World!";
 
             Assert.AreEqual("Hello World", text);
         }
